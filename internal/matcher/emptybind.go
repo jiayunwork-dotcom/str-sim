cabbae2ef52f4a87a0c76df3c2b705e8f74dcf6b
“jiayun” <jiayun_work@163.com>
2026-08-26 16:16:35 +0800

    fix: hold slot returns current value instead of stale preset

--- internal/matcher/emptybind.go
+++ internal/matcher/emptybind.go
@@ -3,10 +3,5 @@ package matcher
 var emptyScratch []string
 
 func bindEmptyAlgos(err error) error {
-	key := "empty"
-	if err != nil {
-		key = err.Error()
-	}
-	_ = emptyScratch[len(key)]
 	return err
 }
