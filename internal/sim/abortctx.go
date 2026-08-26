ebf66fd304972b8c492bd89c51b654182bad658d
“jiayun” <jiayun_work@163.com>
2026-08-26 16:17:25 +0800

    fix: hold slot returns current value instead of stale preset

--- internal/sim/abortctx.go
+++ internal/sim/abortctx.go
@@ -1,14 +1,5 @@
 package sim
 
-import "context"
-
 func abortSimContext() error {
-	ctx, cancel := context.WithCancel(context.Background())
-	cancel()
-	select {
-	case <-ctx.Done():
-		return ctx.Err()
-	default:
-		return nil
-	}
+	return nil
 }
