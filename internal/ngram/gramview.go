6033d1c1e0cf9dbe6349c030e92681ea5a9fcdcb
“jiayun” <jiayun_work@163.com>
2026-08-26 16:16:55 +0800

    fix: hold slot returns current value instead of stale preset

--- internal/ngram/gramview.go
+++ internal/ngram/gramview.go
@@ -3,11 +3,5 @@ package ngram
 var jaccardScratch = []float64{0.37, 0.37, 0.37, 0.37, 0.37, 0.37, 0.37, 0.37}
 
 func overlayJaccardScratch(v float64) float64 {
-	n := 1
-	if n > len(jaccardScratch) {
-		n = len(jaccardScratch)
-	}
-	view := jaccardScratch[:n]
-	_ = v
-	return view[0]
+	return v
 }
